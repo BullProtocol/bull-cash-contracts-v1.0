129853286d14907eba96a11f040883aff939cf14
BCash <admin@bcash.site>
2021-01-12 04:58:06 -0500

    bcash.site

---
+++ Cash.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.6.0;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol';
+import './owner/Operator.sol';
+
+contract Cash is ERC20Burnable, Operator {
+    /**
+     * @notice Constructs the Bcash ERC-20 contract.
+     */
+    constructor() public ERC20('BCASH', 'BCASH') {
+        // Mints 1 Bcash to contract creator for initial Uniswap oracle deployment.
+        // Will be burned after oracle deployment
+        _mint(msg.sender, 1 * 10**18);
+    }
+
+    /**
+     * @notice Operator mints bcash to a recipient
+     * @param recipient_ The address of recipient
+     * @param amount_ The amount of bcash to mint to
+     * @return whether the process has been done
+     */
+    function mint(address recipient_, uint256 amount_)
+        public
+        onlyOperator
+        returns (bool)
+    {
+        uint256 balanceBefore = balanceOf(recipient_);
+        _mint(recipient_, amount_);
+        uint256 balanceAfter = balanceOf(recipient_);
+
+        return balanceAfter > balanceBefore;
+    }
+
+    function burn(uint256 amount) public override onlyOperator {
+        super.burn(amount);
+    }
+
+    function burnFrom(address account, uint256 amount)
+        public
+        override
+        onlyOperator
+    {
+        super.burnFrom(account, amount);
+    }
+}
